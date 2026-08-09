0ebba6ad23a465e85d2dac25cd38234dbf8610c5
Sanika Sangvai <sanikasangvai@gmail.com>
2026-08-09 19:13:44 +0530

    Add Anagram solution

--- anagram.cpp
+++ anagram.cpp
@@ -1,25 +1,24 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-
-    string s1,s2;
-
-    cin>>s1>>s2;
-
-    if(s1.length()!=s2.length())
-    {
-        cout<<"Not Anagram";
+int main(){
+    string s1, s2;
+    cout<<"Enter first string: ";
+   cin>>s1;
+    cout<<"Enter second string: ";
+    cin>>s2;
+    if(s1.length()!=s2.length()){
+        cout<<"Not Anagram strings"<<endl;
         return 0;
     }
 
-    sort(s1.begin(),s1.end());
-    sort(s2.begin(),s2.end());
-
-    if(s1==s2)
-        cout<<"Anagram";
-    else
-        cout<<"Not Anagram";
-
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+    
+    if(s1==s2){
+        cout<<"Strings are Anagram"<<endl;
+    }
+    else{
+        cout<<"Strings are not Anagram"<<endl;
+    }
+    return 0;
 }
