4d78bd83f5c1726591c7917e13aafd271b3a7f63
Sanika Sangvai <sanikasangvai@gmail.com>
2026-08-12 17:35:10 +0530

    Add C++ solution

---
+++ countevenodd.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int even=0,odd=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]%2==0){
+            even++;
+        }
+        else{
+            odd++;
+        }
+    }
+    cout<<"Even elements: "<<even<<endl;
+    cout<<"Odd elements: "<<odd<<endl;
+}
