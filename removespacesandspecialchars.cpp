727e673fb849a6d06bb87b903a29928ec377b20a
Sanika Sangvai <sanikasangvai@gmail.com>
2026-08-13 23:48:39 +0530

    Add C++ solution

---
+++ removespacesandspecialchars.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cout<<"Enter string: ";
+    getline(cin, s);
+    for(int i=0; i< s.length(); i++){
+        if((s[i]>='A' && s[i]<= 'Z') || (s[i]>='a' && s[i]<= 'z') || (s[i]>='0' && s[i]<= '9')){
+            cout<<s[i];
+        }
+    }
+    return 0;
+}
