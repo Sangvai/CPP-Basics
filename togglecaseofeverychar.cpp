507eacb163c7eb7f272df0416854c6ae20547001
Sanika Sangvai <sanikasangvai@gmail.com>
2026-08-14 00:32:46 +0530

    Add C++ solution

---
+++ togglecaseofeverychar.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cout<<"Enter string: ";
+    getline(cin, s);
+    for(int i=0; i<s.length(); i++){
+        if(s[i]>='A'&& s[i]<='Z'){
+            s[i]= s[i]+32;
+        }
+        else if(s[i]>='a' && s[i]<='z'){
+            s[i]=s[i]-32;
+        }
+       
+    }
+     cout<<s;
+    
+    return 0;
+}
