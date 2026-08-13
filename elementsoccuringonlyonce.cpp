b0de4c27fe9d0c365d151a19e8bc75c906ba20b9
Sanika Sangvai <sanikasangvai@gmail.com>
2026-08-13 16:35:46 +0530

    Add C++ solution

---
+++ elementsoccuringonlyonce.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+    int n, arr[100];
+    cout<<"Enter number of array elements: ";
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    for(int i=0; i<n; i++){
+        int count=0;
+        for(int j=0; j<n; j++){
+            if(arr[i]==arr[j]){
+                count++;
+            }
+        }
+    
+    if(count == 1){
+        cout<<arr[i]<<" ";
+    }
+}
+    return 0;
+
+ }
---
+++ elemetnsoccuringonce.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello Sanika"<<endl;
+    cout<<"all is well!!!!!!!!!!";
+    return 0;
+}
